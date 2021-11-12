418012bb533dff0d241e47dd629acc9819a5ffdb
vlsilver <68173811+vlsilver@users.noreply.github.com>
2021-11-13 00:59:28 +0700

    Create TokenFactory Contract
    docs: adds truffle framework infomation,
    feat: adds TokenFactory contract use truffle framework.
    test: adds test for TokenFactory contract use truffle framework.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# This is project to learn create a Dapp.
 
+1. Create, deploy smart contract.
+- [**Rruffle Tutorial**](https://www.trufflesuite.com/tutorial) 
+  ```
+  $ truffle create contract TokenFactory
+  $ truffle complie
+  $ truffle create migration deploy_token_factory
+  $ truffle create test TokenFactory
+  $ truffle migrate
+  $ truffle test
+  ```
+
+- **`TokenFactory`** contract to handle create New token.
+
+- **`PoolSaleToken`** contract to handle salce New token.
---
+++ truffle/test/token_factory.js
@@ -0,0 +1,29 @@
+const TokenFactory = artifacts.require("TokenFactory");
+const Token = artifacts.require("Token");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("TokenFactory", function (accounts) {
+  var newToken
+  it("should create KEEY token", async function () {
+    const factory = await TokenFactory.deployed();
+    await factory.create("Bye bye Token", "KEEYS", "bafybeigzbuwqnhho6skbkz3vgivpoth4e7ipu6a5cebx3jaequ27y7g4ae", 0, 2500);
+    newAddressToken = await factory.tokenOf(accounts[0])
+    newToken = await Token.at(newAddressToken)
+    assert.equal("Bye bye Token", await newToken.name())
+    assert.equal("KEEYS", await newToken.symbol())
+    assert.equal("bafybeigzbuwqnhho6skbkz3vgivpoth4e7ipu6a5cebx3jaequ27y7g4ae", await newToken.tokenUrl())
+    assert.equal(2500, await newToken.totalSupply())
+    assert.equal(0, await newToken.decimals())
+    assert.equal(2500, await newToken.balanceOf(accounts[0]))
+  });
+
+  it("should transfer KEEY token", async function () {
+    let result = await newToken.transfer(accounts[1], 100, { from: accounts[0] })
+    assert.equal(100, await newToken.balanceOf(accounts[1]))
+    assert.equal(2400, await newToken.balanceOf(accounts[0]))
+  });
+});
