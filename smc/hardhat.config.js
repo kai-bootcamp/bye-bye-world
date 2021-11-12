c8ecc859d50f99d143d499662d22a00947eb1603
Nguyễn Ngọc Minh Huy <nnmhuy1999@gmail.com>
2021-11-12 21:23:26 +0700

    Create KEEY token with transferring functions

---
+++ smc/hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomiclabs/hardhat-waffle");
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "vRcdnpikDaQ02o0wc4KoHJ5MiJPwlqQa";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "d86316c9ef10198be8bd4d3b48d536d06a342cc9673c1617d7658e5323f61333";
+
+module.exports = {
+  solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
+};
