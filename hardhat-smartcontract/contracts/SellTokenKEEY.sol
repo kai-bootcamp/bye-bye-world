98598bc7ec838517ca79208f666767d5cedd511a
Duc Trinh <huu-duc.trinh-luong@ekino.com>
2021-12-25 02:22:12 +0700

    fix(contract): New public method + fix number type

--- hardhat-smartcontract/contracts/SellTokenKEEY.sol
+++ hardhat-smartcontract/contracts/SellTokenKEEY.sol
@@ -3,18 +3,22 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SellKEEYToken {
-    uint16 constant private _rate = 10000;
+    uint16 private constant _rate = 10000;
 
     // All these addresses are deployed on Ropsten testnet
     address constant USDT_ADDRESS = 0xdb113b28Fd4E7E772F56cfE938AebA165BD2C47A;
     address constant KEEY_ADDRESS = 0x20DF1Ac97c5f6d9236f2Cfd1D185bF6ef86B8FAB;
     address constant WALLET = 0x157a13F043E596759278Ba916020A353c40C02Af;
 
-    function buyKEEYToken(uint16 number) public payable {
+    function getAmountToPay(uint128 number) public pure returns (uint256) {
+        return number * _rate;
+    }
+
+    function buyKEEYToken(uint128 number) public payable {
         IERC20 usdtToken = IERC20(USDT_ADDRESS);
         IERC20 keeyToken = IERC20(KEEY_ADDRESS);
 
         keeyToken.transferFrom(WALLET, msg.sender, number);
-        usdtToken.transferFrom(msg.sender, WALLET, number * _rate);
+        usdtToken.transferFrom(msg.sender, WALLET, getAmountToPay(number));
     }
 }
